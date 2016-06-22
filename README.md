49f0630627e319f8a94980c07572ad13f7e67f70
zzzhan <imailer@sina.com>
2016-06-23 00:39:42 +0800

    refactor:replace the function name 'beforeQuery' with 'skip'

--- README.md
+++ README.md
@@ -22,62 +22,69 @@ var options = {
 require('mysql-queries').init(options);
 ```
 Use it to some other module, like this:
+
+* Execute SQLs directly
+
 ```js
-var sqlclient = require('mysql-queries');
-//Execute multiple SQLs, like this:
-    sqlclient.queries(['SELECT * FROM prod_unit WHERE NAME=? limit 1',
-	  'INSERT INTO prod_unit(name) values(?)',
-	  'INSERT INTO product(name, type_id, unit_id, price) VALUES(?, ?, ?, ?)'],
-	  [[data.unit_name],[data.unit_name],[data.name,data.type_id,data.unit_id,data.price]], 
-	  function(err, results){
-	    if(!err) {
-		//If not error, the "results" parameter is the results of the SQLs as array.
-	      console.log(results);
-	    } else {
-	      console.log(err);
-	    }
-	  });
-	
-//Or execute multiple SQLs with option, like this:
-    sqlclient.queries(['SELECT * FROM prod_unit WHERE NAME=? limit 1',
-	  'INSERT INTO prod_unit(name) values(?)',
-	  'INSERT INTO product(name, type_id, unit_id, price) VALUES(?, ?, ?, ?)'],
-	  [[data.unit_name],[data.unit_name],[data.name,data.type_id,data.unit_id,data.price]], {
-	  beforeQuery:function(i, arg, results) {
-		var skip = false;
-		switch(i) {
-		  case 1:
-		  //handle second SQL
-		  //Execute the second SQL depending on the first SQL result.
-		  skip = results[0].length!==0;
-		  break;
-		case 2:
-		  //If the second SQL executed, passing the "insertId" to the third SQL as parameter.
-		  if(results[0].length===0) {
-			arg[2]=results[1].insertId;
-		  }
-		  break;
-		}
-		return skip;
+var sqlclient = require('mysql-queries'),
+  sqls = ['SELECT * FROM prod_unit WHERE NAME=? limit 1',
+  'INSERT INTO prod_unit(name) values(?)',
+  'INSERT INTO product(name, type_id, unit_id, price) VALUES(?, ?, ?, ?)'];
+  
+sqlclient.queries(sqls,
+  [[data.unit_name],[data.unit_name],[data.name,data.type_id,data.unit_id,data.price]], 
+  function(err, results){
+	if(!!err) {
+	  console.log(err);
+	} else {
+	//If not error, the "results" is the results of the SQLs as array.
+	  console.log(results);
+	}
+  });
+```
+
+* Execute SQLs with condiction
+
+```js
+sqlclient.queries(sqls,
+  [[data.unit_name],[data.unit_name],[data.name,data.type_id,data.unit_id,data.price]], {
+  skip:function(i, arg, results) {
+	var skip = false;
+	switch(i) {
+	  case 1:
+	  //handle second SQL
+	  //Execute the second SQL depending on the first SQL result.
+	  skip = results[0].length!==0;
+	  break;
+	case 2:
+	  //If the second SQL executed, passing the "insertId" to the third SQL as parameter.
+	  if(results[0].length===0) {
+		arg[2]=results[1].insertId;
 	  }
-	}, function(err, results){
-	  if(!err) {
-		//If not error, the parameter "results" is the results of the SQLs as array.
-		console.log(results);
-	  } else {
-		console.log(err);
-	  }
-	});
-	
-//Or execute with only one SQL, like this:
-	sqlclient.query('SELECT * FROM prod_unit', function(err, result){
-	  if(!err) {
-		//If not error, the parameter "result" is the result of the SQL.
-	    console.log(result);
-	  } else {
-	    console.log(err);
-	  }
-	});
+	  break;
+	}
+	return skip;
+  }
+}, function(err, results){
+  if(!!err) {
+	console.log(err);
+  } else {
+	//If not error, the "results" is the results of the SQLs as array.
+	console.log(results);
+  }
+});
+```
+
+* Execute only one SQL
+
+```js
+sqlclient.query('SELECT * FROM prod_unit', function(err, result){
+  if(!!err) {
+	console.log(err);
+  } else {
+	console.log(result);
+  }
+});
 ```
 
 ## Features
--- test/test.js
+++ test/test.js
@@ -2,61 +2,76 @@ var env = process.env.NODE_ENV || 'development',
   mysqlConfig = require('./mysql.json'),
   cfg = mysqlConfig[env]||mysqlConfig,
   sqlclient = require('../lib/mysql-queries').init(cfg),
-  id='root';
-sqlclient.queries(['SELECT host FROM user where user=? limit 1',
+  id='root',
+  sqls = ['SELECT host FROM user where user=? limit 1',
   'SELECT user FROM user where user=? limit 1',
-  'SELECT password FROM user where user=? limit 1'], [[id],[id],[id]], {
-	  beforeQuery:function(i, arg, results) {
-		var skip = false;
-		switch(i) {
-		  case 1:
-		    //whether skip the update or not,according to the first query result
-			skip = results[0].length===0;
-			break;
-		  case 2:
-		    //whether skip the delete or not,according to the first query result
-			skip = results[0].length!==0;
-			break;
-		}
-		return skip;
-	  }
-  }, function(err, results){
-	  if(!!err) {
-		console.error(err);
-	  } else {
-		console.log(results);
-	  }
-  });
- sqlclient.queries(['SELECT host FROM user where user=? limit 1',
-  'SELECT user FROM user where user=? limit 1',
-  'SELECT password FROM user where user=? limit 1'], [[id],[id],[id]], {
-	  beforeQuery:function(i, arg, results) {
-		var skip = false;
-		switch(i) {
-		  case 1:
-		    //whether skip the update or not,according to the first query result
-			skip = results[0].length===0;
-			break;
-		  case 2:
-		    //whether skip the delete or not,according to the first query result
-			//skip = results[0].length!==0;
-			throw 'break with error.';
-			break;
-		}
-		return skip;
-	  }
-  }, function(err, results){
-	  if(!!err) {
-		console.error(err);
-		console.error(results);
-	  } else {
-		console.log(results);
-	  }
-  }); 
+  'SELECT password FROM user where user=? limit 1'];
+//Sample1:Execute multiple SQLs directly
+sqlclient.queries(sqls, [[id],[id],[id]], function(err, results){
+  if(!!err) {
+	console.error(err);
+  } else {
+	console.log('SQLs Executed directly results(Sample1):');
+	console.log(results);
+	console.log('');
+  }
+});
+
+//Sample2:Execute multiple SQLs with condiction
+sqlclient.queries(sqls, [[id],[id],[id]], {
+  skip:function(i, arg, results) {
+	var skip = false;
+	switch(i) {
+	  case 1:
+		//whether skip the update or not,according to the first query result
+		skip = results[0].length===0;
+		break;
+	  case 2:
+		//whether skip the delete or not,according to the first query result
+		skip = results[0].length!==0;
+		break;
+	}
+	return skip;
+  }
+}, function(err, results){
+  if(!!err) {
+	console.error(err);
+  } else {
+	console.log('Executed with condiction results(Sample2):');
+	console.log(results);
+	console.log('');
+  }
+});
+//Sample3:Throw a error to break the execution
+sqlclient.queries(sqls, [[id],[id],[id]], {
+  skip:function(i, arg, results) {
+	var skip = false;
+	switch(i) {
+	  case 1:
+		skip = results[0].length===0;
+		break;
+	  case 2:
+		throw 'Throw a error to break the execution.';
+		//break;
+	}
+	return skip;
+  }
+}, function(err, results){
+  if(!!err) {
+	console.error('Sample error message(Sample3):');
+	console.error(err);
+	console.log('');
+  } else {
+	console.log(results);
+  }
+}); 
+//Sample4:Execute only one SQL
 sqlclient.query('SELECT host FROM user where user=? limit 1', [id], function(err, results){
   if(!!err) {
 	console.error(err);
   } else {
-	console.log(results);
+	console.log('Only one SQL executed results(Sample4):');
+	console.log(results[0]);
+	console.log('');
   }
 });
